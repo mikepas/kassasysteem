082cf8970c27fbe69cfb98ab3e6f37a3c441321e
Pas, Mike <PS164043@summacollege.nl>
2017-10-25 23:17:15 +0200

    Edited dashboard.xaml and Dashboard.xaml.cs

--- kassasysteem/Dashboard.xaml.cs
+++ kassasysteem/Dashboard.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.ApplicationModel.Core;
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -9,11 +12,34 @@ namespace kassasysteem
         public Dashboard()
         {
             InitializeComponent();
+            tbFocus.Focus(FocusState.Programmatic);
+            //tbFocus.Visibility = Visibility.Collapsed;
         }
 
         private void BtExit_OnClick(object sender, RoutedEventArgs e)
         {
             CoreApplication.Exit();
+        }
+
+        private async void BtSearch_OnClick(object sender, RoutedEventArgs e)
+        {
+            var newView = CoreApplication.CreateNewView();
+            var newViewId = 0;
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                var frame = new Frame();
+                frame.Navigate(typeof(SearchPage), null);
+                Window.Current.Content = frame;
+                Window.Current.Activate();
+
+                newViewId = ApplicationView.GetForCurrentView().Id;
+            });
+            await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).Focus(FocusState.Programmatic);
         }
     }
 }
--- kassasysteem/LoginPage.xaml.cs
+++ kassasysteem/LoginPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -8,7 +10,6 @@ namespace kassasysteem
 {
     public sealed partial class MainPage : Page
     {
-
         public MainPage()
         {
             InitializeComponent();
@@ -17,47 +18,68 @@ namespace kassasysteem
 
         private void getAuthorization()
         {
-            string request = Constants.BASE_URI + "/api/oauth2/auth" +
-                            "?client_id={" + Constants.CLIENT_ID + "}" +
-                            "&redirect_uri=" + Constants.CALLBACK_URL +
-                            "&state=" + Constants.CLIENT_STATE +
-                            "&response_type=code";
+            var request = Constants.BASE_URI + "/api/oauth2/auth" +
+                          "?client_id={" + Constants.CLIENT_ID + "}" +
+                          "&redirect_uri=" + Constants.CALLBACK_URL +
+                          "&state=" + Constants.CLIENT_STATE +
+                          "&response_type=code";
 
             webBrowser.Source = new Uri(request);
             webBrowser.Focus(FocusState.Programmatic);
         }
 
-        private void GetCode(string url)
+        private async void GetCode(string url)
         {
             if (url.IndexOf(Constants.BASE_URI) < 0)
             {
-                int c = url.IndexOf("?code=");
-                int s = url.IndexOf("&state=");
+                var c = url.IndexOf("?code=");
+                var s = url.IndexOf("&state=");
                 OAuth.Code = url.Substring(c + 6, s - c - 6);
                 OAuth.State = url.Substring(s + 7);
+
                 Frame.Navigate(typeof(Dashboard), 0);
+
+                var newView = CoreApplication.CreateNewView();
+                var newViewId = 0;
+                await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    var frame = new Frame();
+                    frame.Navigate(typeof(CustomerPage), null);
+                    Window.Current.Content = frame;
+                    Window.Current.Activate();
+
+                    newViewId = ApplicationView.GetForCurrentView().Id;
+                });
+                var viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
             }
         }
 
         private async void WebView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
         {
             GetCode(args.Uri.ToString());
-            string script = "document.documentElement.style.overflow ='hidden';"
-                            + "document.body.style.backgroundColor = 'green';"
-                            + "document.getElementById('LoginForm').style.width='400px';"
-                            + "document.getElementById('LoginForm').style.align='center';"
-                            + "document.getElementById('LoginForm').style.marginTop='-100px';"
-                            + "document.getElementById('LoginButton').style.background='white';"
-                            + "document.getElementById('LoginButton').style.color='black';"
-                            + "document.getElementsByClassName('enhanced-navigation__top')[0].style.visibility = 'hidden';"
-                            + "document.getElementsByClassName('enhanced-navigation__bottom')[0].style.visibility = 'hidden';"
+            var script = "document.documentElement.style.overflow ='hidden';"
+                         + "document.body.style.backgroundColor = 'green';"
+                         + "document.getElementById('LoginForm').style.width='400px';"
+                         + "document.getElementById('LoginForm').style.align='center';"
+                         + "document.getElementById('LoginForm').style.marginTop='-100px';"
+                         + "document.getElementById('LoginButton').style.background='white';"
+                         + "document.getElementById('LoginButton').style.color='black';"
+                         + "document.getElementsByClassName('enhanced-navigation__top')[0].style.visibility = 'hidden';"
+                         + "document.getElementsByClassName('enhanced-navigation__bottom')[0].style.visibility = 'hidden';"
                 ;
             try
             {
-                WebView wb = (WebView)sender;
-                await wb.InvokeScriptAsync("eval", new string[] { script });
+                var wb = sender;
+                await wb.InvokeScriptAsync("eval", new[] {script});
             }
-            catch { }
+            catch
+            {
+            }
+        }
+
+        private void WebBrowser_OnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            GetCode(args.Uri.ToString());
         }
     }
 }
