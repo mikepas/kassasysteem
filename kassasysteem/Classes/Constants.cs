52a1377f96c01dfa80cdeb86d8e714bf0fb56c67
Pas, Mike <PS164043@summacollege.nl>
2017-10-25 10:06:25 +0200

    Created and designed LoginPage.xaml

---
+++ kassasysteem/Classes/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kassasysteem.Classes
+{
+    static class Constants
+    {
+        // API key = C#  
+        public const string BASE_URI = "https://start.exactonline.nl";
+        public const string CLIENT_ID = "d56dafdc-5e7a-4f6e-bf2a-e531c8c1ff3f";
+        public const string CLIENT_SECRET = "Abf7OAofe2d7";
+        public const string CLIENT_STATE = "1wzxvfh2naf3vvkj2lprxyda";
+        public const string CALLBACK_URL = "http://localhost/exactGLAccountSamplephp/callback.php";
+    }
+}
---
+++ kassasysteem/LoginPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using kassasysteem.Classes;
+
+namespace kassasysteem
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            getAuthorization();
+        }
+
+        private void getAuthorization()
+        {
+            string request = Constants.BASE_URI + "/api/oauth2/auth" +
+                            "?client_id={" + Constants.CLIENT_ID + "}" +
+                            "&redirect_uri=" + Constants.CALLBACK_URL +
+                            "&state=" + Constants.CLIENT_STATE +
+                            "&response_type=code";
+
+            webBrowser.Source = new Uri(request);
+            webBrowser.Focus(FocusState.Programmatic);
+        }
+
+        private void GetCode(string url)
+        {
+            if (url.IndexOf(Constants.BASE_URI) < 0)
+            {
+                int c = url.IndexOf("?code=");
+                int s = url.IndexOf("&state=");
+                //OAuth.Code = url.Substring(c + 6, s - c - 6);
+                //OAuth.State = url.Substring(s + 7);
+                //Frame.Navigate(typeof(GLAList), 0);
+            }
+        }
+
+        private async void WebView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            GetCode(args.Uri.ToString());
+            string script = "document.documentElement.style.overflow ='hidden';"
+                            + "document.body.style.backgroundColor = 'green';"
+                            + "document.getElementById('LoginForm').style.width='400px';"
+                            + "document.getElementById('LoginForm').style.align='center';"
+                            + "document.getElementById('LoginForm').style.marginTop='-100px';"
+                            + "document.getElementById('LoginButton').style.background='white';"
+                            + "document.getElementById('LoginButton').style.color='black';"
+                            + "document.getElementsByClassName('enhanced-navigation__top')[0].style.visibility = 'hidden';"
+                            + "document.getElementsByClassName('enhanced-navigation__bottom')[0].style.visibility = 'hidden';"
+                ;
+            try
+            {
+                WebView wb = (WebView)sender;
+                await wb.InvokeScriptAsync("eval", new string[] { script });
+            }
+            catch { }
+        }
+
+        private void WebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            GetCode(args.Uri.ToString());
+        }
+    }
+}
